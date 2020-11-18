3f1e01561d792ef6af85db2e15e912c6391d243c
Przemek9x <60879687+Przemek9x@users.noreply.github.com>
2020-11-18 10:10:16 +0100

    Add files via upload

---
+++ zadanie1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zadanie1;
+
+/**
+ *
+ * @author przem
+ */
+public class zadanie1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String name = "Przemysław Lewszyk";
+        int age = 26;
+        double annualPay = 300000;
+        
+        System.out.println("Nazywam się " + name + "," + " mam " + age + " lat "
+                         + "i chce zarabiać " + annualPay + " złotych rocznie.");
+        
+    }
+        
+                
+                
+    
+}
