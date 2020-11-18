f23b0ee105bc8810573eed71b528f5690050219d
Przemek9x <60879687+Przemek9x@users.noreply.github.com>
2020-11-18 10:20:46 +0100

    Add files via upload

---
+++ Zadanie2.java
@@ -0,0 +1,20 @@
+package zadanie2;
+
+public class Zadanie2
+{
+    public static void main(String[] args)
+    {
+        String firstName = "Przemek", middleName = "Robert", lastName = "Lewszyk";
+        char firstInitial, middleInitial, lastInitial;
+        
+        firstInitial = firstName.charAt(0);
+        middleInitial = middleName.charAt(0);
+        lastInitial = lastName.charAt(0);
+        
+        System.out.println(firstInitial);
+        System.out.println(middleInitial);
+        System.out.println(lastInitial);
+        
+                
+    }
+}
