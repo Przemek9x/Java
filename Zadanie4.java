671b15344f91f0a5d813a353a7b4cc4e002de2d2
Przemek9x <60879687+Przemek9x@users.noreply.github.com>
2020-11-18 10:40:31 +0100

    Add files via upload

---
+++ Zadanie4.java
@@ -0,0 +1,15 @@
+package zadanie4;
+
+public class Zadanie4
+{
+    public static void main(String[] args)
+    {
+        System.out.print("    *\n");
+        System.out.print("   ***\n");
+        System.out.print("  *****\n");
+        System.out.print(" *******\n");
+        System.out.print("  *****\n");
+        System.out.print("   ***\n");
+        System.out.println("    *");
+    }
+}
