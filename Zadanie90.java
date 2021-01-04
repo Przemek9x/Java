e696bef5adcbbc6810cb7554e9e9230ed5ed9938
Przemysław Lewszyk <przemyslaw.lewszyk@gmail.com>
2021-01-04 17:05:23 +0100

    Metody

---
+++ Zadanie90.java
@@ -0,0 +1,15 @@
+package zadanie90;
+
+
+public class Zadanie90 {
+
+    public static void myName()
+    {
+        System.out.println("Przemek");
+    }
+    public static void main(String[] args) {
+        System.out.println("Wyświetlanie metody myName: ");
+        myName();
+    }
+    
+}
